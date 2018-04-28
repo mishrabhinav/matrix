bdef1fadaf564d8a0ed328978babc1a592012831
Abhinav Mishra <mishrabhinav96@gmail.com>
2018-04-27 23:40:54 -0700

    Pass user settings in the runner

--- runner.py
+++ runner.py
@@ -7,21 +7,22 @@ from os import environ as env
 from pprint import pformat
 
 from pymodm import connect
+from pymodm.errors import DoesNotExist
 
-from core import listify, enumerize
+from core import listify, enumerize, DEFAULT_SETTINGS
 from models import *
 
 
-def _process_recommendations(recommendation):
+def _process_recommendations(recommendation, settings):
     forecasts = [forecast.data for forecast in recommendation.forecast]
-    return enumerize(recommendation.selected.data['legs'][0], forecasts)
+    return enumerize(recommendation.selected.data['legs'][0], forecasts, settings)
 
 
-def _generate_rules_for_user(recs):
+def _generate_rules_for_user(recs, settings):
     tracts = []
     for rec in recs:
         if rec.selected and rec.forecast:
-            tracts.append(_process_recommendations(rec))
+            tracts.append(_process_recommendations(rec, settings))
 
     if not tracts:
         return
@@ -36,7 +37,7 @@ def _read_existing_rules():
 
         formatted_rules = pformat(filtered_rules)
         logging.info(
-            'Reading {} rules for user <{}>, created at {}\n{}'.format(len(filtered_rules), rules.user.username,
+            'Reading {} rules for user <{}>, created at {}\n{}'.format(len(filtered_rules), rules.user,
                                                                        rules.created_on, formatted_rules))
 
 
@@ -46,15 +47,21 @@ def _generate_rules():
         logging.info('Generating rules for user <{}>'.format(user.username))
         recommendations = Recommendations.objects.raw({'user': user.username})
 
-        user_rules = _generate_rules_for_user(recommendations)
+        try:
+            settings = Settings.objects.get({'username': user.username})
+        except DoesNotExist:
+            logging.warn('No settings found for <{}>, using default settings'.format(user.username))
+            settings = DEFAULT_SETTINGS
+
+        user_rules = _generate_rules_for_user(recommendations, settings)
         if user_rules:
             gen_rules.append(
                 Rules(user=user.username, rules=pickle.dumps(user_rules), created_on=datetime.utcnow()))
         else:
             logging.warning('Not enough history for user <{}>'.format(user.username))
 
-    if gen_rules:
-        Rules.objects.bulk_create(gen_rules)
+    # if gen_rules:
+    #     Rules.objects.bulk_create(gen_rules)
 
 
 def main():
@@ -65,7 +72,7 @@ def main():
     connect(env['MONGODB_URI'], alias="recommend-ahp")
     logging.info('Connected to MongoDB')
 
-    # _generate_rules()
+    _generate_rules()
     _read_existing_rules()
 
 
